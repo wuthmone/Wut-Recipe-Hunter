1953d65e1061f681f67bc4e6dcebc31e1ebb3c8c
wuthmone <whlaing10@gmail.com>
2016-04-01 20:04:37 +0800

    make changes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe
 	include HTTParty
+	
 	key_value = ENV['FOOD2FORK_KEY'] || "c01b841ba3c096dafd59675944c2e01b"
 	hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'food2fork.com'
 	base_uri "http://food2fork.com/api"
